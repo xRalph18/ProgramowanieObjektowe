76caaa5071b4ae32f97236f1025630ec6f11dce0
xRalph18 <rafal.machnik4@gmail.com>
2023-04-12 11:04:41 +0200

    Lekcja17 - Events

---
+++ Lekcja17 - Events/Program.cs
@@ -0,0 +1,51 @@
+﻿delegate void EventHandler(object sender, EventArgs e);
+
+class OrderEventArgs : EventArgs
+{
+    public string Email { get; set; }
+    public string Phone { get; set; } 
+}
+
+class Order
+{
+    public event EventHandler<OrderEventArgs> OnCreated;
+
+    public void Create(string email, string phone)
+    {
+        Console.WriteLine("Order created");
+
+        if (OnCreated != null)
+        {
+            OnCreated(this, new OrderEventArgs { Email = email, Phone = phone});
+        }
+    }
+}
+
+class Email
+{
+    public static void Send(object sender, OrderEventArgs e)
+    {
+        Console.WriteLine($"Send an email to {e.Email}");
+    }
+}
+
+class SMS
+{
+    public static void Send(object sender, OrderEventArgs e)
+    {
+        Console.WriteLine($"Send an SMS to {e.Phone}");
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        var order = new Order();
+
+        order.OnCreated += Email.Send;
+        order.OnCreated += SMS.Send;
+
+        order.Create("john@test.com", "(408)-111-2222");
+    }
+}
